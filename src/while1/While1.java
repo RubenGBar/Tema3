67a1f6c3dd821c447e1b8820d353709dd4337cb1
RubenGBar <ruben.garcia@iesnervion.es>
2023-10-11 10:23:54 +0200

    11/10/2023

--- src/while1/While1.java
+++ src/while1/While1.java
@@ -4,22 +4,36 @@ import java.util.Scanner;
 
 public class While1 {
 	public static void main(String[] args) {
-		int suma = 0;
-		int numero;
 		
+		//Declaro las variables donde sumaré los números y donde los voy a guardar
+		int suma = 0, numero;
+		
+		//Creo el Scanner
 		Scanner sc = new Scanner(System.in);
 		
+		//Le pido al usuario que introduzca un número y le digo lo que ocurre si es negativo o positivo
 		System.out.println("Introduzca un valor positivo para sumarlo, si quiere dejar de sumar introduzca un valor negativo");
 		
+		//Leo el valor de numero del teclado
 		numero = sc.nextInt();
 		
+		//Ceo el loop para sumar los números
 		while (numero >= 0) {
+			
+			//Sumo y asigno el valor añadido anteriormente a la variable suma
 			suma += numero;
+			
+			//Vuelvo a pedir al usuario que introduzca un número y le explico lo que ocurre si es negativo o positivo
 			System.out.println("Introduzca un valor positivo para sumarlo, si quiere dejar de sumar introduzca un valor negativo");
+			
+			//Leo el valor de numero del teclado
 			numero = sc.nextInt();
 		}
 		
+		//Después de termianr el bucle muestro el resultado total de la suma
 		System.out.println("El resultado de la suma es: " + suma);
+		
+		//Cierro el Scanner
 		sc.close();
 	}
 
---
+++ src/while2/While2.java
@@ -0,0 +1,40 @@
+package while2;
+
+import java.util.Scanner;
+
+public class While2 {
+	public static void main(String[] args) {
+	
+		/*Declaro las variables donde guardo el número que introduce el usuario, una para sumar los números dentro del bucle
+		y otra para ir aumentandola por cada iteración del bucle*/
+		int num1, cont = 0;
+		
+		//Creo el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//Le pido que introduzca un número y le digo lo que ocurre si es negativo o positivo
+		System.out.println("Introduzca un número: ");
+		
+		//Leo el valor de num1 del teclado
+		num1 = sc.nextInt();
+		
+		//Creo el bucle que hirá iterando hasta que el usuario quiera acabar
+		while (num1 >= 0) {
+			
+			//Le pido al usuario que introduzca un número
+			System.out.println("Introduce un número: ");
+			
+			//Vuelvo a leer el valor de num1 del teclado
+			num1 = sc.nextInt();
+			
+			//Voy sumando la variable cont para saber cuantos número positivos ha introducido
+			cont = cont+1;
+		}
+		
+		//Cuando termine el bucle muestro las veces que ha iterado
+		System.out.println("Ha introducido " + cont + " número/s positivo/s");
+		
+		//Cierro el Scanner
+		sc.close();
+	}
+}
