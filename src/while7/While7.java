21ff407f0db7536842c1e305fb08cffb7f1e2880
RubenGBar <ruben.garcia@iesnervion.es>
2023-10-17 11:12:21 +0200

    17/10/2023

--- src/while7/While7.java
+++ src/while7/While7.java
@@ -19,18 +19,26 @@ public class While7 {
 		
 		if (num < 100) {
 			while (num != random) {
-				if (num < random){
+				if (num == -1){
+					System.out.println("Vaya, te rindes? Había pensado en el número: " + random);
+					System.exit(0);
+				} else if (num == 0){
+					System.out.println("El valor introducido es erróneo");
+					System.exit(0);
+				} else if (num < random){
 					System.out.println("El número es mayor");
 					System.out.println("Te rindes?");
 					num = sc.nextInt();
 					if (num == -1) {
-						System.out.println("Vaya, te rindes? Habí pensado en el número: " + random);
+						System.out.println("Vaya, te rindes? Había pensado en el número: " + random);
+						System.exit(0);
 					}
 				} else if (num > random){
 					System.out.println("El número es menor");
 					System.out.println("Te rindes?");
 					if (num == -1) {
-						System.out.println("Vaya, te rindes? Habí pensado en el número: " + random);
+						System.out.println("Vaya, te rindes? Había pensado en el número: " + random);
+						System.exit(0);
 					}
 					num = sc.nextInt();
 				}
@@ -38,8 +46,6 @@ public class While7 {
 		if (num == random) {
 			System.out.println("Acertaste!!");
 		}
-		} else {
-			System.out.println("El número introducido no es correcto");
 		}
 		
 		sc.close();
