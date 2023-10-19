273c01eba0ceba61853fb87ed006c2779d8b896a
RubenGBar <ruben.garcia@iesnervion.es>
2023-10-19 12:15:59 +0200

    19/10/2023

---
+++ src/do_while5/Do_while5.java
@@ -0,0 +1,17 @@
+package do_while5;
+
+import java.util.Scanner;
+
+public class Do_while5 {
+	public static void main(String[] args) {
+		
+		int num;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+	}
+
+}
