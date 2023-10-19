501d58256d25dfffc34e65364c8b04fc64a7bbca
RubenGBar <ruben.garcia@iesnervion.es>
2023-10-19 13:50:46 +0200

    19/10/2023

--- src/do_while5/Do_while5.java
+++ src/do_while5/Do_while5.java
@@ -1,15 +1,25 @@
 package do_while5;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Do_while5 {
 	public static void main(String[] args) {
 		
-		int num;
+		int num = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		
+		Random rand = new Random();
 		
+		int numeroR = rand.nextInt(1, 101);
+		
+		do {
+			
+			
+			
+		}while (numeroR != num);
+		System.out.println(num);
 		
 		sc.close();
 	}
