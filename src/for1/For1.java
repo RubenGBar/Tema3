8e9e76d46f1760cbcc3d16ec03bb25376ffaf3d9
RubenGBar <ruben.garcia@iesnervion.es>
2023-10-24 14:42:18 +0200

    empezando for1

---
+++ src/for1/For1.java
@@ -0,0 +1,20 @@
+package for1;
+
+import java.util.Scanner;
+
+public class For1 {
+	public static void main(String[] args) {
+		
+		int numero;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca un número: ");
+		
+		numero = sc.nextInt();
+		
+		
+		sc.close();
+	}
+
+}
