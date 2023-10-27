5557b8c8732df3330adac545cef9be3881bff62e
RubenGBar <ruben.garcia@iesnervion.es>
2023-10-27 14:42:28 +0200

    intento9

---
+++ src/for9/For9.java
@@ -0,0 +1,25 @@
+package for9;
+
+import java.util.Scanner;
+
+public class For9 {
+	public static void main(String[] args) {
+		
+		int primo, i;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca un número para comprobar si es primo o no: ");
+		
+		primo = sc.nextInt();
+		
+		for (i = 1; i <= primo; i++) {
+			if (primo / 1 == primo && primo / primo == 1 && primo / i != 0) {
+				
+			}
+		}
+		
+		sc.close();
+	}
+
+}
